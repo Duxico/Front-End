04d003d956a3b251a39c91b757699e2f0a5f6a88
Smallten <smallten@smallten.me>
2023-08-23 16:27:00 +0800

    feat: add nuxt-lodash

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
-  modules: ['@nuxtjs/tailwindcss', 'tdesign-vue-next/es/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'tdesign-vue-next/es/nuxt', 'nuxt-lodash'],
   build: {
     transpile: ['tdesign-vue-next'],
   },
