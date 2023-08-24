2cb80e9409c62b8143d7ea41dadfe49f52f5d8ba
Adam7066 <45750540+Adam7066@users.noreply.github.com>
2023-08-24 16:44:25 +0800

    [Feat] 專案初始建置 (#1)
    * feat: add Nuxt3
    
    * feat: add eslint and prettier
    
    * feat: add tailwindcss
    
    * feat: add husky and lint-staged
    
    * feat: add tdesign
    
    * feat: add nuxt-lodash
    
    * feat: add NuxtImage
    
    * feat: add pinia and persistedstate
    
    * feat: add vueuse

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'tdesign-vue-next/es/nuxt',
+    'nuxt-lodash',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+  ],
+  build: {
+    transpile: ['tdesign-vue-next'],
+  },
+  pinia: {
+    autoImports: ['defineStore'],
+  },
+})
